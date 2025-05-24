e4c60eed1bdac868183cc41b091365b8ed997a50
LuchoLorenzo1 <LuchoLorenzo1@users.noreply.github.com>
2025-05-24 14:40:43 -0300

    feat: setting up postgres with sqlc

---
+++ src/db/init.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT now()
+);
--- src/main.go
+++ src/main.go
@@ -1,22 +1,68 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	repository "gin/db/generated"
+
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
 )
 
-func setupRouter() *gin.Engine {
+func setupRouter(conn repository.DBTX) *gin.Engine {
 	r := gin.Default()
+	queries := repository.New(conn)
+	ctx := context.Background()
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(http.StatusOK, "pong")
+	})
+
+	r.GET("/users", func(c *gin.Context) {
+		users, err := queries.ListUsers(ctx)
+		if err != nil {
+			c.String(http.StatusBadRequest, "Error")
+			return
+		}
+		log.Println(users)
+		c.JSON(http.StatusOK, users)
+	})
+
+	r.POST("/users", func(c *gin.Context) {
+		var userParams repository.CreateUserParams
+
+		if err := c.ShouldBind(&userParams); err != nil {
+			c.String(http.StatusBadRequest, err.Error())
+			return
+		}
+
+		user, err := queries.CreateUser(ctx, userParams)
+		if err != nil {
+			log.Println(err)
+			c.String(http.StatusBadRequest, "Error creating the user")
+			return
+		}
+
+		c.JSON(http.StatusOK, user)
 	})
 
 	return r
 }
 
 func main() {
-	r := setupRouter()
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	fmt.Fprintf(os.Stdout, "Connection to database established successfully\n")
+
+	r := setupRouter(conn)
 	r.Run(":8080")
 }
