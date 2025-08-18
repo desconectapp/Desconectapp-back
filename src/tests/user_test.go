2b249be1f7e77e6d02d77fbfe15bf41a7607135e
Martu <mlozano@fi.uba.ar>
2025-08-18 19:19:06 -0300

    new test

--- src/tests/user_test.go
+++ src/tests/user_test.go
@@ -5,19 +5,17 @@ import (
 	"log"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 
 	"gin/router"
 	"gin/service"
-	
+
 	controller "gin/controller"
 )
 
-const (
-	USERS_NUMBER_FILE = 11
-)
 
 func TestGetUserList(t *testing.T) {
 	router := router.NewRouter()
@@ -43,5 +41,49 @@ func TestGetUserList(t *testing.T) {
 	log.Println(response)
 	
 	assert.Equal(t, w.Code, http.StatusOK, "Status code should be 200")
-	assert.Equal(t, USERS_NUMBER_FILE, len(response.Users))
+	assert.Equal(t, limit, strconv.Itoa(len(response.Users)))
+	assert.Equal(t, false, response.HasMore)
+}
+
+func TestGetUserListPagination(t *testing.T) {
+	router := router.NewRouter()
+	r :=  router.SetupRoutes()
+
+	token, err := service.NewTestToken(1)
+
+	assert.Equal(t, err, nil, "Error should be nil")
+
+	limit := "6"
+	offset := "0"
+
+	req := httptest.NewRequest("GET", "/users?limit="+limit+"&offset="+offset, nil)
+	req.Header.Add("Authorization", "Bearer "+token)
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+
+	var response controller.PaginatedUsers
+
+	err = json.Unmarshal(w.Body.Bytes(), &response)
+	assert.Equal(t, err, nil, "Error should be nil")
+	
+	assert.Equal(t, w.Code, http.StatusOK, "Status code should be 200")
+	assert.Equal(t, limit, strconv.Itoa(len(response.Users)))
+	assert.Equal(t, true, response.HasMore)
+
+	offset_2 := limit
+
+	req_2 := httptest.NewRequest("GET", "/users?limit="+limit+"&offset="+offset_2, nil)
+	req_2.Header.Add("Authorization", "Bearer "+token)
+	w_2 := httptest.NewRecorder()
+	r.ServeHTTP(w_2, req_2)
+
+	var response_2 controller.PaginatedUsers
+
+	err = json.Unmarshal(w_2.Body.Bytes(), &response_2)
+	assert.Equal(t, err, nil, "Error should be nil")
+
+	
+	assert.Equal(t, w_2.Code, http.StatusOK, "Status code should be 200")
+	assert.LessOrEqual(t, limit, strconv.Itoa(len(response.Users)))
+	assert.Equal(t, false, response_2.HasMore)
 }
